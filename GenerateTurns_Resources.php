8002a14bbf19f75a2ba1254e683886c5d4a61e1c
megaforce <matjaz.zibert2@gmail.com>
2016-12-09 14:25:56 +0100

    Add files via upload

---
+++ GenerateTurns_Resources.php
@@ -0,0 +1,12 @@
+<?php
+function GenerateTurns()
+{
+$Turns=rand(10,15); //number of turns
+}
+function GenerateResources()
+{
+	
+	$Money=rand(500,1000);//generates the ammount of starting capital
+	
+}
+?>
